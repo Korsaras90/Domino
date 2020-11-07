ae1e5324af623cc7f8f1415e61281d304bf13adb
Korsaras90 <sergbirukof@gmail.com>
2020-11-07 21:11:52 +0300

    решение

--- src/Dom.java
+++ src/Dom.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Dom {
     public static void main(String[] args) {
-        
+
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+
+        System.out.println(m*n/2);
     }
 }
